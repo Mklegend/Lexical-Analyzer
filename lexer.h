5e140b40ee31fcb1bc98df64e2cc1d559551d976
Mklegend <qureshimoazzam7@gmail.com>
2022-09-19 17:49:57 -0400

    Progress#1

---
+++ lexer.h
@@ -0,0 +1,64 @@
+#ifndef _LEXER_H_
+#define _LEXER_H_
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+//all your tokens goes here
+enum class TokenType
+{
+	END_OF_FILE = 0,
+	ERROR,
+	Identifier,
+	Variable,
+	Number,
+	Letter,
+	String,
+	Comment,
+	AssignmentOperator,
+	RelationalOperator,
+	ArithematicOperator,
+	BlockStart,
+	BlockEnd,
+	SpecialCharacter,
+	Keyword
+};
+//structure of a token 
+struct token
+{
+	string lexeme;
+	TokenType tokenType;//enum type
+	//constructor
+	token(string lexeme, TokenType tokenType);
+	//constructor default
+	token();
+	void Print();
+};
+class lexer
+{
+	vector<char> stream;  //used for storing file sample_code.ol content
+	vector<token> tokens; //vector to store all (tokens,lexeme) pairs
+	void Tokenize();//populates tokens vector
+	int index;
+
+public:
+	lexer();
+	lexer(const char filename[]);
+	void printRaw();//just print everything as it is present in file
+	token getNextToken();//get next token
+	void resetPointer();//reset pointer to start getting tokens from start
+	int getCurrentPointer();//get where current pointer in tokens vector is
+	void setCurrentPointer(int pos);//move current pointer to wherever
+	token peek(int);//peek the next token
+	
+	// Helper Functions
+	bool isLetter(char);
+
+	// Functions for DFAs
+	void identifierDFA(int); // Checks for Identifier Given the Position of the Pointer
+};
+
+#endif // !_LEXER_H
---
+++ test.c
@@ -0,0 +1,14 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+
+int isLetter(char ch){
+    return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) ? 1 : 0;
+}
+
+int main(char*args[],char *argv[]){
+
+    printf("%d",isLetter('A'));
+    printf("%d",isLetter('$'));
+    return 0;
+}
